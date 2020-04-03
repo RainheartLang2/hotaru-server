5771c57f2f49fdec85fc36781cbf5c88c7a77eaf
Vladislav Tisov <RainheartLang@gmail.com>
2020-04-03 13:07:12 +0300

    Initial commit

---
+++ src/main/java/com/hotaru/beans/User.java
@@ -0,0 +1,53 @@
+package com.hotaru.beans;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String middleName;
+
+    @Column
+    private String lastName;
+
+    public User() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/hotaru/rest/HelloWorldService.java
@@ -0,0 +1,17 @@
+package com.hotaru.rest;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@GET
+	@Path("/test")
+	public String getMsg() {
+		return "hello";
+	}
+ 
+}
