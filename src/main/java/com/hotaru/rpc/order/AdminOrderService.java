e7fa2eff9e1c1566fcf592b24e3041cc594dc35d
Vladislav Tisov <RainheartLang@gmail.com>
2020-08-30 17:11:55 +0300

    Обновление статуса заказа

--- src/main/java/com/hotaru/rpc/order/AdminOrderService.java
+++ src/main/java/com/hotaru/rpc/order/AdminOrderService.java
@@ -1,5 +1,6 @@
 package com.hotaru.rpc.order;
 
+import com.hotaru.business.enums.OrderState;
 import com.hotaru.database.entities.Order;
 import com.hotaru.database.resources.OrderResource;
 
@@ -9,5 +10,12 @@ import java.util.List;
 public class AdminOrderService {
     public List<Order> getList(Date from, Date to) {
         return OrderResource.getInstance().getByDateRange(from, to);
+    }
+
+    public void updateState(int id, OrderState newState) {
+        OrderResource resource = OrderResource.getInstance();
+        Order order = resource.getById(id);
+        order.setOrderState(newState);
+        resource.saveOrUpdate(order);
     }
 }
